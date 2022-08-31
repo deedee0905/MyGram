b196f73a79978e4c92e605f8d817ba6d6faed6eb
DESKTOP-8H5ME3U\남상우 <deedee0905@naver.com>
2022-08-31 14:47:03 +0900

    [남상아]  - 프로젝트 최초 파일 등록

---
+++ src/test/java/com/amita/mygramSNS/ex/MygramProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amita.mygramSNS.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygramProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
